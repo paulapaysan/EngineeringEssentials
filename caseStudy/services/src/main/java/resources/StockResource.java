37b63ac3f688dd5df1ab87985ae3934514fa4490
Paula Paysan <paula.paysan@gmail.com>
2019-05-15 17:10:53 -0500

    Update StockResource.java

--- caseStudy/services/src/main/java/resources/StockResource.java
+++ caseStudy/services/src/main/java/resources/StockResource.java
@@ -13,14 +13,36 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
 
 package resources;
 
 // TODO - add your @Path here
 public class StockResource {
 
-    // TODO - Add a @GET resource to get stock data
-    // Your service should return data based on 3 inputs
-    // Stock ticker, start date and end date
+    // resource to get stock data
+    // return data based on 3 inputs
+    // (Stock ticker, start date and end dates)
 
+    public Stock getAllStockPrices(String stockTicker, String start, String end) throws Exception {
+        List<Stock> stocks = FileHelper.readAllStocks("historicalStockData.json");
+        List<ClosePrice> dailyClosePrices = new ArrayList<ClosePrice>();
+        // look for company
+        for (Stock company : companies) {
+            // if stock data found, create close price list
+            if (stockTicker.equals(company.name)) {
+                dailyClosePrices = company.data;
+                return dailyClosePrices; 
+            }
+        }
+        // stock ticker not found
+        Response r = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error: Stock Ticker not found.").build();
+        System.out.println(r.toString());
+    }
 }
